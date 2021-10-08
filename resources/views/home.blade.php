4bf62d18b86542b9e51c207899cb23bc64fcabf2
MDimasPrimaA <dimasprima1212@gmail.com>
2021-10-09 00:03:29 +0800

    fisrt commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>UTS WEB FRAMEWORK
+</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0">
+<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<link href='http://fonts.googleapis.com/css?family=Coda' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Jura:400,500,600,300' rel='stylesheet' type='text/css'>
+<script src="js/jquery-1.8.0.min.js"></script>
+<script src="js/jquery.touchwipe.1.1.1.js"></script>
+<script src="js/jquery.carouFredSel-5.5.0-packed.js"></script>
+<!--[if lt IE 9]><script src="js/modernizr.custom.js"></script><![endif]-->
+<script src="js/functions.js"></script>
+</head>
+<body>
+<!-- wrapper -->
+<div class="wrapper">
+  <!-- header -->
+  <header class="header">
+    <div class="shell">
+      <div class="header-top">
+        <nav id="navigation"> <a href="#" class="nav-btn">Home<span></span></a>
+          <ul>
+            <li class="active home"><a href="#">Beranda</a></li>
+            <li><a href="#">Servis</a></li>
+            <li><a href="#">Testimoni</a></li>
+            <li><a href="#">Hubungi Kami</a></li>
+            <li><a href="#">About</a></li>
+          </ul>
+        </nav>
+        <div class="cl">&nbsp;</div>
+      </div>
+      <div class="slider">
+        <div id="bg"></div>
+        <div id="carousel">
+          <div>
+            <h5>Servis HP</h5>
+            <h3>Jenis HP</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget augue quis quam dignissim consectetur ac sit amet nisl. In hac habitasse platea. Eget augue quis quam dignissi lorem lipsum adispicing dolor sit amet.</p>
+                <img class="img-front" src="css/images/front-img.png" alt=""> <img class="img-mid" src="css/images/img-mid.png" alt=""> <img class="img-back" src="css/images/img-back.png" alt=""> </div>
+          <div>
+            <h5>Testimoni</h5>
+            <h3>Baik</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget augue quis quam dignissim consectetur ac sit amet nisl. In hac habitasse platea. Eget augue quis quam dignissi lorem lipsum adispicing dolor sit amet.</p>
+                <img class="img-front" src="css/images/front-img.png" alt=""> <img class="img-mid" src="css/images/img-mid.png" alt=""> <img class="img-back" src="css/images/img-back.png" alt=""> </div>
+          <div>
+            <h5>Hubungi kami</h5>
+            <h3>Great Vision</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget augue quis quam dignissim consectetur ac sit amet nisl. In hac habitasse platea. Eget augue quis quam dignissi lorem lipsum adispicing dolor sit amet.</p>
+                <img class="img-front" src="css/images/front-img.png" alt=""> <img class="img-mid" src="css/images/img-mid.png" alt=""> <img class="img-back" src="css/images/img-back.png" alt=""> </div>
+          <div>
+            <h5>About</h5>
+            <h3>Great Vision</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget augue quis quam dignissim consectetur ac sit amet nisl. In hac habitasse platea. Eget augue quis quam dignissi lorem lipsum adispicing dolor sit amet.</p>
+                <img class="img-front" src="css/images/front-img.png" alt=""> <img class="img-mid" src="css/images/img-mid.png" alt=""> <img class="img-back" src="css/images/img-back.png" alt=""> </div>
+        </div>
+        <div class="pagination"></div>
+        <a id="prev" href="#"></a> <a id="next" href="#"></a> </div>
+    </div>
+  </header>
+  <!-- end of header -->
+  <!-- shell -->
+  
+  </div>
+  <!-- end of shell -->
+  <!-- footer -->
+  <div id="footer">
+    <!-- shell -->
+    <div class="shell">
+      <!-- footer-cols -->
+      <div class="footer-cols">
+        <div class="col">
+          <h2>SERVIS</h2>
+          <ul>
+          <li><a href="#">Servis Asus</a></li>
+            <li><a href="#">Servis Xiaomi</a></li>
+            <li><a href="#">Servis Iphone</a></li>
+            <li><a href="#">Servis Samsung</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h2>TESTIMONI</h2>
+          <ul>
+            <li><a href="#">Baik</a></li>
+            <li><a href="#">Lumayan</a></li>
+            <li><a href="#">Buruk</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <h2>HUBUNGI KAMI</h2>
+          <ul>
+            <li><i class="fa fa-phone-alt">081351761118</a></li>
+            <li><i class="fa fa-envelope">dimasprima1212@gmail.com </a></li>
+            <li><i class="fa fa-map-marker-alt">123 Street, New York, USA</a></li>
+          </ul>
+        
+        </div>
+        <div class="cl">&nbsp;</div>
+      </div>
+      <!-- end of footer-cols -->
+      <div class="footer-bottom">
+        <div class="footer-nav">
+          <ul>
+            <li><a hrerf="#">Beranda</a></li>
+            <li><a hrerf="#">Servis</a></li>
+            <li><a hrerf="#">Testimoni</a></li>
+            <li><a hrerf="#">Hubungi kami</a></li>
+            <li><a hrerf="#">About</a></li>
+          </ul>
+        </div>
+        <p class="copy">Copyright &copy; 2012<span>|</span>Design by: <a target="_blank" href="http://chocotemplates.com">ChocoTemplates</a></p>
+        <div class="cl">&nbsp;</div>
+      </div>
+    </div>
+    <!-- end of shell -->
+  </div>
+  <!-- footer -->
+</div>
+<!-- end of wrapper -->
+</body>
+</html>
